da29872b111789da6d75a7f904424448c7f942f0
SUSHILxSPAM <100059120+SUSHILxSPAM@users.noreply.github.com>
2022-02-24 06:35:47 +0530

    Update __main__.py

--- RiZoeLXSpam/__main__.py
+++ RiZoeLXSpam/__main__.py
@@ -1,20 +1,20 @@
-#RiZoeLXSpam By @TheRiZoeL
+#RcRXSpam By @RCRoWNER
 
 import asyncio
 import sys
 from sys import argv
 import glob
 from pathlib import Path
-from RiZoeLXSpam.utils import load_plugins
+from RcRXSpam.utils import load_plugins
 import logging
 from telethon import events
-from . import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, Riz21, Riz22, Riz23, Riz24, Riz25, Riz26, Riz27, Riz28, Riz29, Riz30, Riz31, Riz32, Riz33, Riz34, Riz35, Riz36, Riz37, Riz38, Riz39, Riz40
+from . import RcR, RcR2, RcR3, RcR4, RcR5 , RcR6, RcR7, RcR8, RcR9, RcR10, RcR11, RcR12, RcR13, RcR14, RcR15, RcR16, RcR17, RcR18, RcR19, RcR20, RcR21, RcR22, RcR23, RcR24, RcR25, RcR26, RcR27, RcR28, RcR29, RcR30, RcR31, RcR32, RcR33, RcR34, RcR35, RcR36, RcR37, RcR38, RcR39, RcR40
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 
-path = "RiZoeLXSpam/plugins/*.py"
+path = "RcRXSpam/plugins/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as a:
@@ -22,104 +22,104 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("RiZoeL Bot Spam Successfully deployed -!")
-print("Enjoy! Do visit @RiZoeLX")
+print("RcR Bot Spam Successfully deployed -!")
+print("Enjoy! Do visit @RCR_OFFICIAL")
 
 if len(argv) not in (1, 3, 4):
     try:
-        Riz.disconnect()
+        RCR.disconnect()
     except Exception as e:
         pass
     try:
-        Riz2.disconnect()
+        RcR2.disconnect()
     except Exception as e:
         pass
     try:
-        Riz3.disconnect()
+        RcR3.disconnect()
     except Exception as e:
         pass
     try:
-        Riz4.disconnect()
+        RcR4.disconnect()
     except Exception as e:
         pass
     try:
-        Riz5.disconnect()
+        RcR5.disconnect()
     except Exception as e:
         pass
     try:
-        Riz6.disconnect()
+        RcR6.disconnect()
     except Exception as e:
         pass
     try:
-        Riz7.disconnect()
+        RcR7.disconnect()
     except Exception as e:
         pass
     try:
-        Riz8.disconnect()
+        RcR8.disconnect()
     except Exception as e:
         pass
     try:
-        Riz9.disconnect()
+        RcR9.disconnect()
     except Exception as e:
         pass
     try:
-        Riz10.disconnect()
+        RcR10.disconnect()
     except Exception as e:
         pass
     try:
-        Riz11.disconnect()
+        RcR11.disconnect()
     except Exception as e:
         pass
     try:
-        Riz12.disconnect()
+        RcR12.disconnect()
     except Exception as e:
         pass
     try:
-        Riz13.disconnect()
+        RcR13.disconnect()
     except Exception as e:
         pass
     try:
-        Riz14.disconnect()
+        RcR14.disconnect()
     except Exception as e:
         pass
     try:
-        Riz15.disconnect()
+        RcR15.disconnect()
     except Exception as e:
         pass
     try:
-        Riz16.disconnect()
+        RcR16.disconnect()
     except Exception as e:
         pass
     try:
-        Riz17.disconnect()
+        RcR17.disconnect()
     except Exception as e:
         pass
     try:
-        Riz18.disconnect()
+        RcR18.disconnect()
     except Exception as e:
         pass
     try:
-        Riz19.disconnect()
+        RcR19.disconnect()
     except Exception as e:
         pass
     try:
-        Riz20.disconnect()
+        RcR20.disconnect()
     except Exception as e:
         pass
     try:
-        Riz21.disconnect()
+        RcR21.disconnect()
     except Exception as e:
         pass
     try:
-        Riz22.disconnect()
+        RcR22.disconnect()
     except Exception as e:
         pass
     try:
-        Riz23.disconnect()
+        RcR23.disconnect()
     except Exception as e:
         pass
     try:
-        Riz24.disconnect()
+        RcR24.disconnect()
     except Exception as e:
         pass
     try:
@@ -127,19 +127,19 @@ if len(argv) not in (1, 3, 4):
     except Exception as e:
         pass
     try:
-        Riz26.disconnect()
+        Rcr26.disconnect()
     except Exception as e:
         pass
     try:
-        Riz27.disconnect()
+        RcR27.disconnect()
     except Exception as e:
         pass
     try:
-        Riz28.disconnect()
+        RcR28.disconnect()
     except Exception as e:
         pass
     try:
-        Riz29.disconnect()
+        RcR29.disconnect()
     except Exception as e:
         pass
     try:
@@ -147,203 +147,203 @@ if len(argv) not in (1, 3, 4):
     except Exception as e:
         pass
     try:
-        Riz31.disconnect()
+        RcR31.disconnect()
     except Exception as e:
         pass
     try:
-        Riz32.disconnect()
+        RcR32.disconnect()
     except Exception as e:
         pass
     try:
-        Riz33.disconnect()
+        RcR33.disconnect()
     except Exception as e:
         pass
     try:
-        Riz34.disconnect()
+        RcR34.disconnect()
     except Exception as e:
         pass
     try:
-        Riz35.disconnect()
+        RcR35.disconnect()
     except Exception as e:
         pass
     try:
-        Riz36.disconnect()
+        RcR36.disconnect()
     except Exception as e:
         pass
     try:
-        Riz37.disconnect()
+        RcR37.disconnect()
     except Exception as e:
         pass
     try:
-        Riz38.disconnect()
+        RcR38.disconnect()
     except Exception as e:
         pass
     try:
-        Riz39.disconnect()
+        RcE39.disconnect()
     except Exception as e:
         pass
     try:
-        Riz40.disconnect()
+        RcR40.disconnect()
     except Exception as e:
         pass
 else:
     try:
-        Riz.run_until_disconnected()
+        SuS.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz2.run_until_disconnected()
+        SuS2.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz3.run_until_disconnected()
+        SuS3.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz4.run_until_disconnected()
+        SuS4.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz5.run_until_disconnected()
+        SuS5.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz6.run_until_disconnected()
+        SuS6.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz7.run_until_disconnected()
+        SuS7.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz8.run_until_disconnected()
+        SuS8.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz9.run_until_disconnected()
+        SuS9.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz10.run_until_disconnected()
+        SuS10.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz11.run_until_disconnected()
+        SuS11.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz12.run_until_disconnected()
+        SuS12.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz13.run_until_disconnected()
+        SuS13.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz14.run_until_disconnected()
+        SuS14.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz15.run_until_disconnected()
+        SuS15.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz16.run_until_disconnected()
+        SuS16.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz17.run_until_disconnected()
+        SuS17.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz18.run_until_disconnected()
+        SuS18.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz19.run_until_disconnected()
+        SuS19.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz20.run_until_disconnected()
+        SuS20.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz21.run_until_disconnected()
+        SuS21.run_until_disconnected()
+    except Exceptihn as e:
+        pass
+    try:
+        SuS22.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz22.run_until_disconnected()
+        SuS23.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz23.run_until_disconnected()
+        SuS24.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz24.run_until_disconnected()
+        SuS25.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz25.run_until_disconnected()
+        SuS26.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz26.run_until_disconnected()
+        SuS27.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz27.run_until_disconnected()
+        SuS28.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz28.run_until_disconnected()
+        SuS29.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz29.run_until_disconnected()
+        SuS30.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz30.run_until_disconnected()
+        SuS31.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz31.run_until_disconnected()
+        SuS32.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz32.run_until_disconnected()
+        SuS33.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz33.run_until_disconnected()
+        SuS34.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz34.run_until_disconnected()
+        SuS35.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz35.run_until_disconnected()
+        SuS36.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz36.run_until_disconnected()
+        SuS37.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz37.run_until_disconnected()
+        SuS38.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz38.run_until_disconnected()
+        SuS39.run_until_disconnected()
     except Exception as e:
         pass
     try:
-        Riz39.run_until_disconnected()
-    except Exception as e:
-        pass
-    try:
-        Riz40.run_until_disconnected()
+        SuS40.run_until_disconnected()
     except Exception as e:
         pass
